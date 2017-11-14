144f79715048d9d7794f7d557fd5d9cfde73fc79
Cyanoth <Cyanoth@users.noreply.github.com>
2017-11-14 14:47:06 +0000

    Initial commit

---
+++ src/MainApplication.java
@@ -0,0 +1,52 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class MainApplication {
+
+    public static void main(String[] args){
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("C:\\Users\\Charlie\\IdeaProjects\\ImageCompression\\res\\LineTest.bmp"));
+            int imgWidth = img.getWidth();
+            int imgHeight = img.getHeight();
+            int pixelArray[][] = new int[imgWidth][imgHeight]; //Instantiate two arrays WxH
+
+            String runLengthEncoding = "";
+            int rangeLimit = 2;
+
+            for (int x = 0; x < imgWidth -3; x++) //TODO: Change from -3 (temp stop out of bounds)
+            {
+                int tmpRunCounter = 1;
+                int curPixelValue = img.getRGB(x, 0);
+                curPixelValue = (curPixelValue >> 16) & 0x000000FF; //[REF 1]
+
+                    for (int ix = x+1; ix < imgWidth -1; ix++) //TODO: change from -1 (temp stop out of bounds)
+                    {
+                        int thisPixelValue = img.getRGB(ix, 0);
+                        thisPixelValue = (thisPixelValue >> 16) & 0x000000FF;
+
+                        if (Math.abs(curPixelValue - thisPixelValue) <= 2)
+                            tmpRunCounter++;
+                        else
+                        {
+                            runLengthEncoding += curPixelValue + "|" + tmpRunCounter + "|";
+                            x = ix;
+                            break;
+                        }
+                    }
+            }
+
+            System.out.println(runLengthEncoding);
+
+
+
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred - Failed to load image!\n" + e);
+        }
+    }
+
+}
