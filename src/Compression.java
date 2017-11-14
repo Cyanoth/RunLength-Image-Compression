89d5dc7c8569db40a4d259224ba755facd7e757c
Cyanoth <Cyanoth@users.noreply.github.com>
2017-11-14 15:33:21 +0000

    Tided Code

--- src/Compression.java
+++ src/Compression.java
@@ -1,2 +1,69 @@
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
+
 public class Compression {
+    private static final int RANGE_LIMIT = 2;
+
+
+    public static void runCompressImage(BufferedImage img)
+    {
+        System.out.println("Running Compression...");
+
+        int imgWidth = img.getWidth();
+        int imgHeight = img.getHeight();
+
+        String runLengthEncoding = "";
+        int y = 0;
+
+        for (int x = 0; x < imgWidth; x++) {
+            int tmpRunCounter = 1;
+            int curPixelValue = getColourLevel(img, 'R', x, y);
+
+            for (int ix = x + 1; ix < imgWidth + 1; ix++) {
+                if (ix == imgWidth) {
+                    x = imgWidth;
+                    break;
+                }
+                else {
+                    int nextPixelValue = getColourLevel(img, 'R', ix, y);
+
+                    if (Math.abs(curPixelValue - nextPixelValue) <= RANGE_LIMIT)
+                        tmpRunCounter++;
+                    else {
+                        x = ix - 1;
+                        break;
+                    }
+                }
+            }
+            runLengthEncoding += curPixelValue + "|" + tmpRunCounter + "|";
+        }
+
+        System.out.println("Compression Finished! String Result:");
+        System.out.println(runLengthEncoding);
+    }
+
+    private static int getColourLevel(BufferedImage img, char getWhichRGB, int x, int y) //todo: should return a byte?
+    {
+        int pixVal = img.getRGB(x, y);
+        int resVal = 0;
+        switch (getWhichRGB) {
+            case 'R':
+                resVal = (pixVal >> 16) & 0xFF; //[REF 1]
+                break;
+            case 'G':
+                resVal = (pixVal >> 8) & 0xFF; //[REF 1]
+                break;
+            case 'B':
+                resVal = pixVal & 0xFF; //[REF 1]
+                break;
+            default:
+                System.out.println("WARNING! Invalid RGB Selector!!!");
+                break;
+
+        }
+
+        return resVal;
+
+    }
+
 }
--- src/MainApplication.java
+++ src/MainApplication.java
@@ -10,41 +10,9 @@ public class MainApplication {
         BufferedImage img = null;
         try {
             img = ImageIO.read(new File("C:\\Users\\Charlie\\IdeaProjects\\ImageCompression\\res\\LineTest.bmp"));
-            int imgWidth = img.getWidth();
-            int imgHeight = img.getHeight();
+            Compression.runCompressImage(img);
 
-            String runLengthEncoding = "";
-            int rangeLimit = 2;
 
-            System.out.println("width is: " + imgWidth);
-
-            for (int x = 0; x < imgWidth; x++) {
-                int tmpRunCounter = 1;
-                int curPixelValue = img.getRGB(x, 0); //TODO: Replace with a function
-                curPixelValue = (curPixelValue >> 16) & 0x000000FF; //[REF 1]
-
-                for (int ix = x + 1; ix < imgWidth + 1; ix++) {
-
-                    if (ix == imgWidth) {
-                        x = imgWidth;
-                        break;
-                    } else {
-                        int thisPixelValue = img.getRGB(ix, 0);
-                        thisPixelValue = (thisPixelValue >> 16) & 0x000000FF;
-
-                        if (Math.abs(curPixelValue - thisPixelValue) <= rangeLimit)
-                            tmpRunCounter++;
-                        else {
-                            x = ix - 1;
-                            break;
-                        }
-                    }
-                }
-
-                runLengthEncoding += curPixelValue + "|" + tmpRunCounter + "|";
-            }
-
-            System.out.println(runLengthEncoding);
 
 
 
